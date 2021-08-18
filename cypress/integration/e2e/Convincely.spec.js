d6df1531378b55e63f594cafb3b6898217122961
james@convincely.com <suvendu.reo@gmail.com>
2021-08-18 11:08:04 +1000

    add test cases for Phone number verification

--- cypress/integration/e2e/Convincely.spec.js
+++ cypress/integration/e2e/Convincely.spec.js
@@ -20,7 +20,15 @@ describe('Sample Test for Convincely', () => {
     })
     it('Verify phone number in convincely page is correct', () => {
         cy.visit('https://wwwstg.aia.com.au/en/individual/health-insurance.html')
-        
+        cy.get("#input-lifeStage")
+        .click()
+        cy.get(".conv-header-phone").click()
+        cy.get(".conv-button-label").then(($phonenumber) => {         
+            const PhNbr = $phonenumber.text();
+            cy.log(PhNbr);
+            cy.expect(PhNbr).to.include("1800 333 004")
+        })
+
     })
     })
     
